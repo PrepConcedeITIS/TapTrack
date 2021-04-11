6911176b528edd4ee3050a00875b2ca4c1b9882d
PrepConcede <44526727+PrepConcedeITIS@users.noreply.github.com>
2021-04-11 20:55:08 +0300

    master fix

--- TapTrackAPI.Core.Features/Issue/Handlers/GetIssueHandler.cs
+++ TapTrackAPI.Core.Features/Issue/Handlers/GetIssueHandler.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using TapTrackAPI.Core.Base;
