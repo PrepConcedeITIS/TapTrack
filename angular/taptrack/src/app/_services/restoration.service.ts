a3426e4bb3e12bc3e24a4e32c00209f7f81f1622
Nikita <treskov-1@mail.ru>
2021-04-13 17:28:25 +0300

    Component sending email (ain't working)

---
+++ angular/taptrack/src/app/_services/restoration.service.ts
@@ -0,0 +1,24 @@
+import {Injectable, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {User} from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestorationService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  SendEmail(userCredentials): Observable<RestorationCode> {
+    return this.httpClient.post<RestorationCode>(`${environment.apiUrl}/Restoration`, userCredentials);
+  }
+}
+
+export interface RestorationCode{
+  UserMail: string;
+}
+
+
--- angular/taptrack/src/app/restoration-email/restoration-email.component.ts
+++ angular/taptrack/src/app/restoration-email/restoration-email.component.ts
@@ -4,6 +4,14 @@ import {MustMatch} from '../_helpers/must-match.validator';
 import {Router} from '@angular/router';
 import {AuthenticationService} from '../_services/authentication.service';
 import {RegistrationService} from '../_services/registration.service';
+import {RestorationService} from "../_services/restoration.service";
+
+export class RestorationCode {
+  constructor(
+    public UserMail?: string,
+    public code?: number
+  ){}
+}
 
 @Component({
   selector: 'app-restoration',
@@ -13,24 +21,29 @@ import {RegistrationService} from '../_services/registration.service';
 export class RestorationEmailComponent implements OnInit {
   form: FormGroup;
   emailControl: AbstractControl;
-
+  // tslint:disable-next-line:new-parens
+  restorationCode: RestorationCode = new RestorationCode;
   constructor(private registrationService: RegistrationService,
               private authenticationService: AuthenticationService,
+              private restorationService: RestorationService,
               private formBuilder: FormBuilder,
               private router: Router) {
   }
 
   ngOnInit(): void {
     this.authenticationService.logout();
-
     this.form = this.formBuilder.group({
       email: ['', [Validators.email, Validators.required]]
     }, {
     });
     this.emailControl = this.form.get('email');
+    this.restorationCode.UserMail = this.form.get('email').value;
   }
 
   submit() {
-
+    if (!this.form.invalid){
+      this.restorationService.SendEmail({UserMail: this.form.get('email').value})
+        .subscribe();
+    }
   }
 }
