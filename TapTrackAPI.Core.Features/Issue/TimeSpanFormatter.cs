54dfda2d82b93d1f2686b3605944aac28b475d56
ravil322 <nasybullin_ravil@mail.ru>
2021-04-18 15:34:24 +0300

    тестовый форматтер

---
+++ TapTrackAPI.Core.Features/Issue/TimeSpanFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TapTrackAPI.Core.Features.Issue
+{
+    public class TimeSpanFormatter
+    {
+        public string Formatter(TimeSpan date)
+        {
+            var weeks = date.Days / 7;
+            var days = date.Days % 7;
+            var hours = date.Hours;
+            var output = weeks + "w " + days + "d " + hours + "h ";
+            return output;
+        }
+    }
+}
