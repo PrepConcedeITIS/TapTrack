34b8394afefdaec84545bc558071cb5bc88f182b
ravil322 <nasybullin_ravil@mail.ru>
2021-04-25 17:37:32 +0300

    добавлен форматтер в обратную сторону

--- TapTrackAPI.Core.Features/Issue/TimeSpanFormatter.cs
+++ TapTrackAPI.Core.Features/Issue/TimeSpanFormatter.cs
@@ -8,13 +8,23 @@ namespace TapTrackAPI.Core.Features.Issue
 {
     public class TimeSpanFormatter
     {
-        public string Formatter(TimeSpan date)
+        public string FormatterFromTimeSpan(TimeSpan date)
         {
             var weeks = date.Days / 5;
             var days = date.Days % 5;
             var hours = date.Hours;
             var output = weeks + "w " + days + "d " + hours + "h ";
             return output;
+        }
+
+        public TimeSpan FormatterFromString(string date)
+        {
+            var splitter = date.Split('w', 'd', 'h');
+            var weeks = int.Parse(splitter[0]);
+            var days = int.Parse(splitter[1]);
+            var hours = int.Parse(splitter[2]);
+            var allDays = weeks * 5 + days;
+            return new TimeSpan(allDays, hours, 0, 0);
         }
     }
 }
