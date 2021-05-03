ccf2b7605531158af283aedfbbb070b2e57a1a3a
PrepConcede <44526727+PrepConcedeITIS@users.noreply.github.com>
2021-05-03 14:42:38 +0300

    TPK-23 routes and login link to pass restoration

--- angular/taptrack/src/app/app-routing.module.ts
+++ angular/taptrack/src/app/app-routing.module.ts
@@ -9,22 +9,26 @@ import {ProjectDetailsComponent} from './project/details/project-details.compone
 import {ProjectUpdateComponent} from './project/update/project-update.component';
 import {ProjectListComponent} from './project/list/project-list.component';
 import {ErrorComponent} from './error/error.component';
-import {IssueDetailsComponent} from "./issue/issue-details/issue-details.component";
-import {RestorationEmailComponent} from "./auth/password-restoration/restoration-email/restoration-email.component";
-import {RestorationCodeComponent} from "./auth/password-restoration/restoration-code/restoration-code.component";
-import {RestorationPasswordComponent} from "./auth/password-restoration/restoration-password/restoration-password.component";
-import {ArticleComponent} from "./knowledge-base/article/article.component";
-import {ArticleDetailsComponent} from "./knowledge-base/article-details/article-details.component";
-import {ArticleCreateComponent} from "./knowledge-base/article-create/article-create.component";
+import {IssueDetailsComponent} from './issue/issue-details/issue-details.component';
+import {RestorationEmailComponent} from './auth/password-restoration/restoration-email/restoration-email.component';
+import {RestorationCodeComponent} from './auth/password-restoration/restoration-code/restoration-code.component';
+import {RestorationPasswordComponent} from './auth/password-restoration/restoration-password/restoration-password.component';
+import {ArticleComponent} from './knowledge-base/article/article.component';
+import {ArticleDetailsComponent} from './knowledge-base/article-details/article-details.component';
+import {ArticleCreateComponent} from './knowledge-base/article-create/article-create.component';
 import {ForbiddenErrorComponent} from './error/forbidden-error/forbidden-error.component';
-import {ArticleUpdateComponent} from "./knowledge-base/article-update/article-update.component";
+import {ArticleUpdateComponent} from './knowledge-base/article-update/article-update.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/project/list', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'restoration-email', component: RestorationEmailComponent},
-  {path: 'restoration-code', component: RestorationCodeComponent},
-  {path: 'restoration-password', component: RestorationPasswordComponent},
+  {
+    path: 'restoration', children: [
+      {path: 'email', component: RestorationEmailComponent},
+      {path: 'code', component: RestorationCodeComponent},
+      {path: 'password', component: RestorationPasswordComponent},
+    ]
+  },
   {path: 'registration', component: RegistrationComponent},
   {
     path: 'issue', canActivate: [AuthGuard], children: [
