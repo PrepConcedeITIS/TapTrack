a592ec4329dc55b40645dc08c20fd4f8f09b1153
Grigorev Yakov <yaschok315@gmail.com>
2021-05-14 08:21:17 +0000

    TPK-144

---
+++ TapTrackAPI.Core.Features/Commenting/DTOs/EditedCommentDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TapTrackAPI.Core.Features.Commenting.DTOs
+{
+    public class EditedCommentDTO
+    {
+        public string Text { get; init; }
+        public DateTime LastUpdated { get; init; }
+    }
+}
---
+++ TapTrackAPI.Core.Features/Commenting/Handlers/EditCommentCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TapTrackAPI.Core.Entities;
+using TapTrackAPI.Core.Features.Commenting.Base;
+using TapTrackAPI.Core.Features.Commenting.Commands;
+using TapTrackAPI.Core.Features.Commenting.DTOs;
+
+namespace TapTrackAPI.Core.Features.Commenting.Handlers
+{
+    public class EditCommentCommandHandler : BaseCommandHandler, IRequestHandler<UpdateCommentCommand, EditedCommentDTO>
+    {
+        public EditCommentCommandHandler(DbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+        {
+        }
+
+        public async Task<EditedCommentDTO> Handle(UpdateCommentCommand request, CancellationToken cancellationToken)
+        {
+            var comment = await DbContext
+                .Set<Comment>()
+                .FindAsync(new object[] {request.Id}, cancellationToken);
+            comment.UpdateText(request.Text);
+            await DbContext.SaveChangesAsync(cancellationToken);
+            return Mapper.Map<EditedCommentDTO>(comment);
+        }
+    }
+}
