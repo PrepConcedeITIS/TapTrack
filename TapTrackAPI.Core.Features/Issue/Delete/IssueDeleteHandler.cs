2041d25a59e0c31871b5b9a3d38f85e79dadbfa9
PrepConcede <44526727+PrepConcedeITIS@users.noreply.github.com>
2021-05-21 19:11:06 +0300

    TPK-119 validation

---
+++ TapTrackAPI.Core.Features/Issue/Delete/IssueDeleteHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using JetBrains.Annotations;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TapTrackAPI.Core.Base;
+
+namespace TapTrackAPI.Core.Features.Issue.Delete
+{
+    [UsedImplicitly]
+    public class IssueDeleteHandler : RequestHandlerBase, IRequestHandler<IssueDeleteCommand>
+    {
+        public IssueDeleteHandler(DbContext dbContext, IMediator mediator, IMapper mapper) : base(dbContext, mapper)
+        {
+        }
+
+        public async Task<Unit> Handle(IssueDeleteCommand request, CancellationToken cancellationToken)
+        {
+            var issues = Context.Set<Entities.Issue>();
+            var issue = issues.FirstOrDefault(x => x.Id == request.IssueId);
+
+            if (issue == null)
+                return Unit.Value;
+
+            issues.Remove(issue);
+            await Context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
