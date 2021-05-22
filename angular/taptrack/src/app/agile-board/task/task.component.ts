d3a13b09890f8651591eba2daf085b8100b6e4d9
PrepConcede <44526727+PrepConcedeITIS@users.noreply.github.com>
2021-05-22 16:53:35 +0300

    TPK-151 ui2

--- angular/taptrack/src/app/agile-board/task/task.component.ts
+++ angular/taptrack/src/app/agile-board/task/task.component.ts
@@ -1,19 +1,17 @@
-import { Component, Input, OnInit } from "@angular/core";
+import {Component, Input, OnInit} from '@angular/core';
 import {IssueDetailsDto} from '../../issue/IssueDetailsDto';
 
 
 @Component({
-    selector: 'app-task',
-    templateUrl: './task.component.html',
-    styleUrls: ['./task.component.scss']
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
 })
 
-export class TaskComponent implements OnInit{
+export class TaskComponent implements OnInit {
 
-    @Input() task: IssueDetailsDto;
+  @Input() task: IssueDetailsDto;
 
-
-    ngOnInit(): void {
-
-    }
+  ngOnInit(): void {
+  }
 }
