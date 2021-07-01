81609dff79d43fd4fa27f096adbf12594db8d142
PrepConcede <tagmirs_letter@mail.ru>
2021-07-02 00:59:26 +0300

    project section

--- TapTrackAPI.Core.Features/Project/List/GetProjectsListAsyncHandler.cs
+++ TapTrackAPI.Core.Features/Project/List/GetProjectsListAsyncHandler.cs
@@ -5,35 +5,30 @@ using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using JetBrains.Annotations;
-using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using TapTrackAPI.Core.Base.Handlers;
 using TapTrackAPI.Core.Entities;
 using TapTrackAPI.Core.Extensions;
 
 namespace TapTrackAPI.Core.Features.Project.List
 {
     [UsedImplicitly]
-    public class GetProjectsListAsyncHandler : IRequestHandler<GetProjectsListQuery, List<ProjectDto>>
+    public class GetProjectsListAsyncHandler : BaseHandlerWithUserManager<GetProjectsListQuery, List<ProjectDto>>
     {
-        private readonly DbContext _dbContext;
-        private readonly IMapper _mapper;
-        private readonly UserManager<User> _userManager;
-
-        public GetProjectsListAsyncHandler(DbContext dbContext, IMapper mapper, UserManager<User> userManager)
+        public GetProjectsListAsyncHandler(DbContext dbContext, IMapper mapper, UserManager<User> userManager) : base(
+            dbContext, mapper, userManager)
         {
-            _dbContext = dbContext;
-            _mapper = mapper;
-            _userManager = userManager;
         }
 
-        public async Task<List<ProjectDto>> Handle(GetProjectsListQuery request, CancellationToken cancellationToken)
+        public override async Task<List<ProjectDto>> Handle(GetProjectsListQuery request,
+            CancellationToken cancellationToken)
         {
-            var userId = _userManager.GetUserIdGuid(request.ClaimsPrincipal);
-            var projectsWhereMember = await _dbContext.Set<TeamMember>()
+            var userId = UserManager.GetUserIdGuid(request.ClaimsPrincipal);
+            var projectsWhereMember = await DbContext.Set<TeamMember>()
                 .Where(x => x.UserId == userId)
                 .Select(x => x.Project)
-                .ProjectTo<ProjectDto>(_mapper.ConfigurationProvider)
+                .ProjectTo<ProjectDto>(Mapper.ConfigurationProvider)
                 .ToListAsync(cancellationToken: cancellationToken);
             return projectsWhereMember;
         }
