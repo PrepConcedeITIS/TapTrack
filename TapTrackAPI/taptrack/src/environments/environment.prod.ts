b19af03b578a877ac0eaeb3e20a6c4ae06066bac
PrepConcede <tagmirs_letter@mail.ru>
2021-07-13 23:48:54 +0300

    Update environment.prod.ts

--- TapTrackAPI/taptrack/src/environments/environment.prod.ts
+++ TapTrackAPI/taptrack/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://www.taptrack.tech/api',
-  tgBotName: 'TapTrackNotificationDebugBot'
+  tgBotName: 'TapTrackNotificationBot'
 };
